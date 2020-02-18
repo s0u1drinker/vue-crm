7261a7d1b7dd6a1d8cc53b801a0a7352874aa5d9
s0u1drinker <norkiw40k@gmail.com>
2020-02-18 15:32:19 +0400

    Create repo and first commit

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,14 @@
 </template>
 
 <style lang="scss">
+@import '@/styles/normalize.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $dark;
 }
 
 #nav {
@@ -22,10 +24,10 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $dark;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $primary;
     }
   }
 }
---
+++ src/views/404.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>404</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  color: $green;
+}
+</style>
