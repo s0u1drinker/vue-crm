4e865d294defc86a0eb3bb8db349b96ea1f88f84
s0u1drinker <norkiw40k@gmail.com>
2020-02-25 16:53:53 +0400

    Extra data removed and layouts files added.

--- src/App.vue
+++ src/App.vue
@@ -10,25 +10,4 @@
 
 <style lang="scss">
 @import '@/styles/normalize.scss';
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $dark;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: $dark;
-
-    &.router-link-exact-active {
-      color: $primary;
-    }
-  }
-}
 </style>
